3cc6c86d546f8c7507a3393617a001825be4397c
Raj Beemi <beemi.raja@gmail.com>
2024-03-30 21:31:43 +0000

    feat: first commit

--- compare_pdf_06.py
+++ compare_pdf_06.py
@@ -73,9 +73,9 @@ def compare_pdfs_highlight_and_combine(pdf_path1, pdf_path2, output_pdf_path):
 
 # Example usage
 # Paths to the PDF files you want to compare
-pdf_path1 = "pdffiles/source/sample_pdf_1.pdf"
-pdf_path2 = "pdffiles/target/sample_pdf_2.pdf"
+pdf_path1 = "pdffiles/source/pdf_uat.pdf"
+pdf_path2 = "pdffiles/target/pdf_prod.pdf"
 
-output_pdf_path = 'output_highlighted_difference_06_03.pdf'
+output_pdf_path = 'output_highlighted_difference_06_04.pdf'
 
 compare_pdfs_highlight_and_combine(pdf_path1, pdf_path2, output_pdf_path)
